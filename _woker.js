1f2becb8bb846e6a5e7695ec6598ac1e57c461c8
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-07-08 16:05:34 +0800

    Update _woker.js

--- _woker.js
+++ _woker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.notion.so'
+      url.hostname = 'notion.so'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
