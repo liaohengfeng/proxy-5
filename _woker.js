504eaaff83e6fd6e5cf3d75df8cddf6a454b5a8a
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-07-18 08:46:18 +0800

    Update _woker.js

--- _woker.js
+++ _woker.js
@@ -1,12 +1,8 @@
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="bzhai.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
-    }
-  };
+  async fetch(request, env) {
+    const _url = new URL(request.url);
+    _url.hostname = "bzhai.com";
+    const req = new Request(_url, request);
+    return fetch(req);
+  },
+};
