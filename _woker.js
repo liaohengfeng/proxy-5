1dcca5a3d4ee905fbe56704695e2f0ff97380d07
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-07-18 08:47:20 +0800

    Update _woker.js

--- _woker.js
+++ _woker.js
@@ -1,8 +1,11 @@
 export default {
   async fetch(request, env) {
-    const _url = new URL(request.url);
-    _url.hostname = "bzhai.com";
-    const req = new Request(_url, request);
-    return fetch(req);
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'bzhai.com'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
   },
 };
